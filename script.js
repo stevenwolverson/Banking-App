f0f360be12b8fd9e01310ed5da5857142fe54df0
Steven Wolverson <119756447+stevenwolverson@users.noreply.github.com>
2023-08-25 20:16:53 +0800

    Update Date Features

--- script.js
+++ script.js
@@ -200,13 +200,7 @@ btnLogin.addEventListener('click', function (e) {
       options
     ).format(now);
 
-    // const now = new Date();
-    // const day = `${now.getDate()}`.padStart(2, 0);
-    // const month = `${now.getMonth() + 1}`.padStart(2, 0);
-    // const year = now.getFullYear();
-    // const hour = `${now.getHours()}`.padStart(2, 0);
-    // const min = `${now.getMinutes()}`.padStart(2, 0);
-    // labelDate.textContent = `${day}/${month}/${year}, ${hour}:${min}`;
+  
 
     // Clear input field
     inputLoginUsername.value = inputLoginPin.value = '';
